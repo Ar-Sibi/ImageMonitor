da30317116f18e5ce048a1d7aab4a6bcf1343e7c
Ar-Sibi <getgoingwithsibi@gmail.com>
2018-05-04 19:09:49 +0530

    Add negative message for empty count

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -10,20 +10,19 @@ import { Activity } from '../activity';
 })
 export class AdminComponent implements OnInit {
   activities: Activity[];
-  countArray: number[];
+  countArray: number[] = [];
   constructor(private imageMonitor: MonitorService,
-    private route:ActivatedRoute,
-    private location:Location
+    private route: ActivatedRoute,
+    private location: Location
   ) {
   }
-  page(pageno){
-    this.imageMonitor.getActivityPaginated(this.route.snapshot.paramMap.get('id'),10*pageno).subscribe( activities => {this.activities=activities;console.log(activities);return activities });
+  page(pageno) {
+    this.imageMonitor.getActivityPaginated(this.route.snapshot.paramMap.get('id'), 10 * pageno).subscribe(activities => { this.activities = activities; console.log(activities); return activities });
   }
 
   ngOnInit() {
-
-    this.imageMonitor.getCount(this.route.snapshot.paramMap.get('id')).subscribe(num=>{console.log(num);this.countArray=Array.apply(null, {length: ((+num-1)/10)+1}).map(Number.call, Number)})
-    this.imageMonitor.getActivityPaginated(this.route.snapshot.paramMap.get('id'),0).subscribe( activities => {this.activities=activities;console.log(activities);return activities });
+    this.imageMonitor.getCount(this.route.snapshot.paramMap.get('id')).subscribe(num => { console.log(num); this.countArray = Array.apply(null, { length: ((+num - 1) / 10) + 1 }).map(Number.call, Number) })
+    this.imageMonitor.getActivityPaginated(this.route.snapshot.paramMap.get('id'), 0).subscribe(activities => { this.activities = activities; console.log(activities); return activities });
   }
 
 }
